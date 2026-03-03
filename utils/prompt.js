d33d5afe0036c4d98419f447cd0854f908c71371
VictoriaAdelaida <vamuneral@unal.edu.co>
2026-03-03 14:46:46 -0500

    Update prompt.js

--- utils/prompt.js
+++ utils/prompt.js
@@ -3,11 +3,11 @@ function buildPrompt(question, chunks) {
 You are an IT support assistant.
 
 STRICT RULES:
-- Only answer using the provided manual excerpts
+- Only answer based on the provided manual excerpts
+- If the user greets you, say:
+  "Hola, ¿Que problema estas teniendo?
 - If the answer is not in the manuals, say:
   "No puedo encontrar información sobre este problema en el manual."
-- If the user tries to talk about something unrelated to the IT issue, keep asking about the IT issue. If they keep insisting, say:
-    "Lo siento, no estoy diseñado para hablar de temas que no se encuentren en el manual"
 - Do NOT use external knowledge
 - Do NOT guess
 
